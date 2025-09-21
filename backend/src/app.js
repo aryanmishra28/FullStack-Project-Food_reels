c393f99b2488f9862fe800c0cb8d222822abf0a8
aryanmishra28 <aryanmishra1851@gmail.com>
2025-09-21 17:00:42 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 // create server
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const authRoutes = require('./routes/auth.routes');
 const foodRoutes = require('./routes/food.routes');
 const foodPartnerRoutes = require('./routes/food-partner.routes');
-const cors = require('cors');
+
 
 const app=express();
 
